e74fb9b8dbda263d2fa16ede844c9f2ab7765534
juanfesba <juanfesba@hotmail.com>
2025-03-08 23:45:06 -0500

    wow

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # GeminiFun
 Just automatizing fun stuff with Gemini
+
+# Creating Go Module
+*  go mod init github.com/juanfesba/gemini-fun
+*  go get github.com/google/generative-ai-go
+
+go run geminifun/main.go
---
+++ geminifun/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/google/generative-ai-go/genai"
+	"google.golang.org/api/option"
+)
+
+func printResponse(resp *genai.GenerateContentResponse) {
+	for _, cand := range resp.Candidates {
+		if cand.Content != nil {
+			for _, part := range cand.Content.Parts {
+				fmt.Println(part)
+			}
+		}
+	}
+	fmt.Println("---")
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+
+	ctx := context.Background()
+	client, err := genai.NewClient(ctx, option.WithAPIKey(os.Getenv("GEMINI_API_KEY")))
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer client.Close()
+
+	// [START text_gen_text_only_prompt]
+	model := client.GenerativeModel("gemini-1.5-flash")
+	resp, err := model.GenerateContent(ctx, genai.Text("Write a story about a magic backpack."))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	printResponse(resp)
+	// [END text_gen_text_only_prompt]
+}
